b4c940be84ab6f9f0ffe3d16a12d7a5cbd196adb
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-17 23:55:46 +0530

    feature: Added handler fn in init_vibe instruction

--- programs/vibe-programs/src/instructions/init_vibe.rs
+++ programs/vibe-programs/src/instructions/init_vibe.rs
@@ -10,5 +10,29 @@ pub struct InitializeVibe<'info> {
     #[account(mut, signer)]
     pub author: AccountInfo<'info>,
 
+    pub clock: Sysvar<'info, Clock>,
     pub system_program: Program<'info, System>,
+}
+
+
+// Initialize an account that will store a Vibe
+pub fn handler(
+    ctx: Context<InitializeVibe>, 
+    vibe_content: Vec<VibeContent>, 
+    allowed_comments: bool
+    ) -> Result<()> {
+
+    let vibe = &mut ctx.accounts.vibe;
+    let author = &mut ctx.accounts.author;
+    let clock = &mut ctx.accounts.clock;
+
+    vibe.author = *author.key;
+    vibe.vibe_content = vibe_content;
+    vibe.version = 0;
+    vibe.likes = 0;
+    vibe.comments = 0;
+    vibe.allowed_comments = allowed_comments;
+    vibe.timestamp = clock.unix_timestamp;
+
+    Ok(())
 }
