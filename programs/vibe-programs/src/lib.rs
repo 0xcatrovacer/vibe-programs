a0a0c7e9996fecd9869f7da5dc47b54d24be5614
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-17 23:56:22 +0530

    flow: Added init_vibe instruction to lib.rs

--- programs/vibe-programs/src/lib.rs
+++ programs/vibe-programs/src/lib.rs
@@ -1,13 +1,26 @@
 use anchor_lang::prelude::*;
 
+pub mod state;
+pub mod instructions;
+
+use state::*;
+use instructions::*;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod vibe_programs {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+
+    // Initialize a Vibe
+    pub fn init_vibe(
+        ctx: Context<InitializeVibe>, 
+        vibe_content: Vec<VibeContent>, 
+        allowed_comments: bool
+    ) -> Result<()> {
+        instructions::init_vibe::handler(ctx, vibe_content, allowed_comments)
     }
+
 }
 
 #[derive(Accounts)]
