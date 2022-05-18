9d3d61d400f3c6f5394e14313630675cba324407
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-18 18:07:54 +0530

    feature: Added handler fn in add_comment

--- programs/vibe-programs/src/instructions/add_comment.rs
+++ programs/vibe-programs/src/instructions/add_comment.rs
@@ -22,3 +22,19 @@ pub struct AddComment<'info> {
     pub system_program: Program<'info, System>,
 }
 
+pub fn handler(
+    ctx: Context<AddComment>,
+    content: String,
+    bump: u8
+) -> Result<()> {
+    let vibe = &mut ctx.accounts.vibe;
+    let comment_account = &mut ctx.accounts.comment;
+
+    let commentor = ctx.accounts.commentor.key;
+
+    comment_account.add_comment(vibe.key(), content, *commentor, bump);
+
+    vibe.comments += 1;
+
+    Ok(())
+}
