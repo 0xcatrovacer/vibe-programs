cfecaf191e7f6b9f05b79cb34ce0c5eaba54c19d
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-24 14:30:12 +0530

    feature: added add_like handler function

--- programs/vibe-programs/src/instructions/add_like.rs
+++ programs/vibe-programs/src/instructions/add_like.rs
@@ -4,11 +4,31 @@ use crate::state::*;
 
 #[derive(Accounts)]
 pub struct AddLike<'info> {
-    #[account(init, payer = liker, space = Like::LEN)]
+    #[account(
+        init,
+        seeds=[liker.key().as_ref(), vibe.key().as_ref()],
+        bump,
+        payer = liker, 
+        space = Like::LEN
+    )]
     pub like: Account<'info, Like>,
     #[account(mut)]
     pub vibe: Account<'info, Vibe>,
     #[account(mut, signer)]
     pub liker: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
+}
+
+pub fn handler (ctx: Context<AddLike>) -> Result<()> {
+
+    let vibe = &mut ctx.accounts.vibe;
+    let like = &mut ctx.accounts.like;
+    let liker = &mut ctx.accounts.liker;
+
+    let bump = *ctx.bumps.get("like").unwrap();
+    vibe.likes += 1;
+
+    like.add_like(vibe.key(), *liker.key, bump);
+
+    Ok(())
 }
