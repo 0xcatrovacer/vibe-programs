216031541e7ae4e5598d25556e248288f07ac871
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-25 17:45:03 +0530

    flow: Added init_user to instructions mod.rs

--- programs/vibe-programs/src/instructions/mod.rs
+++ programs/vibe-programs/src/instructions/mod.rs
@@ -1,3 +1,4 @@
+pub mod init_user;
 pub mod init_vibe;
 pub mod add_comment;
 pub mod remove_comment;
@@ -5,6 +6,7 @@ pub mod add_like;
 pub mod remove_like;
 pub mod follow_user;
 
+pub use init_user::*;
 pub use init_vibe::*;
 pub use add_comment::*;
 pub use remove_comment::*;
