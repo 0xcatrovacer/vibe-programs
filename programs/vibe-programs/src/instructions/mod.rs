3810562095f8214e6df01a50fe952990dbb337cb
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-25 18:46:13 +0530

    flow: Added remove_like to instruction mod

--- programs/vibe-programs/src/instructions/mod.rs
+++ programs/vibe-programs/src/instructions/mod.rs
@@ -1,5 +1,6 @@
 pub mod init_user;
 pub mod init_vibe;
+pub mod remove_vibe;
 pub mod add_comment;
 pub mod remove_comment;
 pub mod add_like;
@@ -13,3 +14,4 @@ pub use remove_comment::*;
 pub use add_like::*;
 pub use remove_like::*;
 pub use follow_user::*;
+pub use remove_vibe::*;
