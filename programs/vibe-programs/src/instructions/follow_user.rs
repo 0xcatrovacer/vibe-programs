2675325abffd94f9042df4b2e9d6ac041c9a1656
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-05-25 18:49:04 +0530

    fix: Wired logic to increase follow numbers in user accounts

--- programs/vibe-programs/src/instructions/follow_user.rs
+++ programs/vibe-programs/src/instructions/follow_user.rs
@@ -16,6 +16,16 @@ pub struct FollowUser<'info> {
     pub followed: AccountInfo<'info>,
     #[account(mut, signer)]
     pub follower: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = followed_account.user_key == *followed.key
+    )]
+    pub followed_account: Account<'info, User>,
+    #[account(
+        mut,
+        constraint = follower_account.user_key == *follower.key
+    )]
+    pub follower_account: Account<'info, User>,
     pub system_program: Program<'info, System>,
 }
 
@@ -25,11 +35,17 @@ pub fn handler(ctx: Context<FollowUser>) -> Result<()> {
     let follower = &mut ctx.accounts.follower;
     let followed = &mut ctx.accounts.followed;
 
+    let followed_account = &mut ctx.accounts.followed_account;
+    let follower_account = &mut ctx.accounts.follower_account;
+
     let bump = *ctx.bumps.get("follow").unwrap();
 
     follow.followed = *followed.key;
     follow.follower = *follower.key;
     follow.bump = bump;
+
+    followed_account.followers += 1;
+    follower_account.followings += 1;
 
     Ok(())
 
